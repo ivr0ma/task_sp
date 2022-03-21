9945fb03c26d54e3959df6981c0ec8e88aa1f7b5
ivr0ma <ivroma.ru@yandex.ru>
2022-03-21 21:25:45 +0300

    changed main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world!")
+import os
+data_name = 'data'  # папка с набором данных
+
+current_path = str(os.getcwd())  # текущий путь
+data_path = os.path.join(current_path, data_name)  # путь к набору данных
+list_app = os.listdir(data_path)  # список приложений
+print("number of applications in the dataset:", len(list_app))
+
+trace_count = {}  # частотный словарь
+trace_all_count = 0  # количество трасс в датасете
+for app in list_app:
+    traces = os.listdir(os.path.join(data_path, app))  # список трасс
+    for trace in traces:  # подсчет трасс
+        if trace in trace_count:
+            trace_count[trace] += 1
+        else:
+            trace_count[trace] = 1
+
+    trace_all_count += len(traces)
+
+print("number of traces in the dataset:", trace_all_count)
+print(trace_count)
